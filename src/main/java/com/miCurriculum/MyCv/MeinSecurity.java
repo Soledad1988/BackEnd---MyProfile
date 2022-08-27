66bf58fd8f9e6d643ef901b37d412efb16f1f10f
Brenda <brentorres26@gmail.com>
2022-08-27 19:44:47 -0300

    BackEnd - mi profile

--- src/main/java/com/miCurriculum/MyCv/MeinSecurity.java
+++ src/main/java/com/miCurriculum/MyCv/MeinSecurity.java
@@ -58,7 +58,7 @@ public class MeinSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
